a534e9dcf037e03e974ba093e35117fe9b78c869
EdwarMartinez <edwarmartinez@yahoo.com>
2022-04-19 13:57:36 -0500

    Initial commit

---
+++ Calculator/CalculatorLogic.swift
@@ -0,0 +1,57 @@
+//
+//  Model.swift
+//  Calculator
+//
+
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    private var number:Double?
+    
+    var intermediateCalculation : (n1: Double, calcMethod: String)?
+    
+    
+    mutating func setNumber(_ number : Double) {
+        self.number = number
+    }
+    
+    mutating func calculate(symbol: String)->Double? {
+        if let n = number{
+            switch symbol{
+                case "+/-":
+                    return n * -1
+                case "AC":
+                    return 0
+                case "%":
+                    return n * 0.01
+                case "=":
+                    return performTwoNumCalculation(n2:n)
+                default:
+                    intermediateCalculation = (n1:n, calcMethod:symbol)
+                    return n
+            }
+        }
+        return nil
+    }
+    
+    private mutating func performTwoNumCalculation(n2: Double)->Double?{
+        if let n1 = intermediateCalculation?.n1,
+           let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("Operation not matched")
+            }
+        }
+        return nil
+    }
+}
